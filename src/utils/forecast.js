6dfce49a8fd77ca886c25427577ceb82d5cc113b
Kunal Dhenge <kdhenge@gmail.com>
2020-12-31 23:54:55 +0530

    updated humidity text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback('Unable to find location!!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree out. It feels like ' + body.current.feelslike + ' degrees out. It is ' + body.current.humidity + ' percent humid.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree out. It feels like ' + body.current.feelslike + ' degrees out. The Humidity is ' + body.current.humidity + '%.')
         }
     })
 }
